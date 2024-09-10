23bf8f9b9315b4e9d026d49da6f046df9a112649
MakarandPundlik <mmp2308@gmail.com>
2024-09-10 00:08:49 -0400

    initial commit

---
+++ main.rb
@@ -0,0 +1,13 @@
+puts("Hello World")
+print("Hello World \n") # \n will add the new line below 
+
+# full_name = first_name + " " + last_name
+#will throw an error
+#undefined local variable or method `first_name' for main (NameError)
+# full_name = first_name + " " + last_name
+# ^^^^^^^^^^
+
+first_name = "Makarand"
+last_name = "Pundlik"
+
+puts first_name + last_name
