e2595e75a512cf94cd9d3d73721e1440cb9c5f8e
MakarandPundlik <mmp2308@gmail.com>
2024-09-10 17:53:59 -0400

    for loops

---
+++ for-loop.rb
@@ -0,0 +1,11 @@
+4.times do |number| 
+  puts "My name is test"
+end 
+
+#goes from 0 to 2 and not from 1 to 3 
+
+(5..10).each do |why|
+  puts "Value of why " + why.to_s
+end
+
+#loop above is more like javascript than JAVA
